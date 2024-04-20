9e7a0673d18f94481ca3b31405c582af7c149b57
Daniel Rolls <daniel.rolls.27@googlemail.com>
2024-04-20 11:10:48 +0100

    Full e2e test calling generated binary

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,27 +1,64 @@
 import Prelude hiding (unlines)
-import Test.Hspec (describe, hspec, it, shouldBe, expectationFailure)
+
+import Control.Monad (when)
+import Data.ByteString (hGetContents)
+import Data.ByteString.Char8 (unpack)
+import Data.List.Extra (splitOn)
 import Data.Text (Text, unlines)
+import qualified Data.Text (unpack)
 import qualified Data.Text.Lazy as LazyText
+import System.Directory (setCurrentDirectory)
+import System.Environment (lookupEnv)
+import System.Exit (ExitCode(ExitFailure, ExitSuccess))
+import System.Process (StdStream(CreatePipe), createProcess, proc, std_err, std_out, waitForProcess)
+import Test.Hspec (describe, expectationFailure, hspec, it, shouldBe)
 
 import SpecUp (invokeTemplateOnSpec)
 
-main = hspec $ do
+main = do
+  runExtendedTests <- testVarSet
+  hspec $ do
 
-    it "should pass new" $
-      invokeTemplateOnSpec testTmpl sampleTestYaml
+    when runExtendedTests $
+        it "should call the binary with the example in the README" $
+          usingTestData "example-from-readme" $
+            do res <- callSpecUpWithArgs "-s animal.yaml -t template.m"
+               specupStandardErr res `shouldBe` ""
+               (specupStandardOut res `shouldBe`) =<< readFile "output.txt"
+               specupExitCode res `shouldBe` ExitSuccess
+
+    it "should work with a template with a simple list" $
+      invokeTemplateOnSpec "{{# .}}# {{{ fieldName }}}\n{{/ .}}"
+                           "- fieldName: value1\n- fieldName: value2"
         `shouldReturnMarkupOf`
       [ "# value1"
       , "# value2"
       ]
 
-shouldReturnMarkupOf output expectedMarkup =
+data SpecupResult = SpecupResult {
+  specupStandardOut :: !String
+, specupStandardErr :: !String
+, specupExitCode :: !ExitCode
+} deriving (Show, Eq)
+
+output `shouldReturnMarkupOf` expectedMarkup =
     either
       (expectationFailure . ("expected Right but got Left " <>))
-      (`shouldBe` resultLines expectedMarkup)
+      (`shouldBe` LazyText.unlines expectedMarkup)
       output
 
-sampleTestYaml = "- fieldName: value1\n- fieldName: value2"
+callSpecUpWithArgs :: String -> IO SpecupResult
+callSpecUpWithArgs args =
+         do (_,Just mOut, Just mErr, procHandle) <- createProcess $ (proc "specup" (splitOn " " args)) {std_out = CreatePipe, std_err = CreatePipe}
+            SpecupResult <$> (unpack <$> hGetContents mOut)
+                         <*> (unpack <$> hGetContents mErr)
+                         <*> waitForProcess procHandle
 
-testTmpl = "{{# .}}# {{{ fieldName }}}\n{{/ .}}"
+testVarSet =
+  (== Just "1") <$> lookupEnv "TEST_BINARY"
 
-resultLines = LazyText.unlines
+usingTestData testData payload =
+  do setCurrentDirectory $ "test/resources/" <> testData
+     res <- payload
+     setCurrentDirectory "../.."
+     return res
